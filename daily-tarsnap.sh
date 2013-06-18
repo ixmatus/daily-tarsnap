7b7d57b49a5459d2517b1ff59fda48cde8d48460
Parnell Springmeyer <ixmatus@gmail.com>
2013-06-18 00:09:09 -0500

    Fixing a small issue with the installation lists

--- daily-tarsnap.sh
+++ daily-tarsnap.sh
@@ -19,19 +19,21 @@ logecho () {
 
 logecho "--- Dumping list of installations"
 
-port installed > /Users/ixmatus/mac_ports_installed.txt
+/opt/local/bin/port installed > /Users/ixmatus/mac_ports_installed.txt
 ls -al /Applications > /Users/ixmatus/mac_dmgs_installed.txt
 
 logecho "--- Executing tarsnap backup"
 
 # Only backing up my home dir as everything else *should* be
 # configurable from what's in my homedir only
-tarsnap -c -f \
+/usr/local/bin/tarsnap -c -f \
 system_backup-`date +\%Y-%m-%d` \
 /Users/ixmatus/Certs \
 /Users/ixmatus/Cloud \
 /Users/ixmatus/Documents \
 /Users/ixmatus/Desktop \
+/Users/ixmatus/mac_dmgs_installed.txt \
+/Users/ixmatus/mac_ports_installed.txt \
 /Users/ixmatus/.* \
 --exclude .vagrant.d .emacs .emacs.d *dialyzer_plt .emacs.elc .fabricrc \
 >> ${DAILYLOG} 2>&1
